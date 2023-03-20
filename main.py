9bf3c15db3d9e90a2dd95e84b4fbaa62a951ed6f
ElHadjiOumar <elhadjimbengueo@gmail.com>
2023-03-20 15:31:58 +0100

    Second Commit

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-# Ce programme ajoute 2 nombre
-
-num1 = 5
-num2 = 2
-
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print(
-    'Pour avoir le predator : La somme de {0} + {1} est {2}'.format(num1, num2, sum))
-
-
 def inc(x):
     return x + 1
 
