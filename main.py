18dbff49eb2dac820a433e73be4d7c51e5ff59b9
ElHadjiOumar <elhadjimbengueo@gmail.com>
2023-03-20 14:35:28 +0100

    Second Commit

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ num2 = 2
 sum = num1 + num2
 
 # Display the sum
-print('Pour avoir le predator : La somme de {0} + {1} est {2}'.format(num1, num2, sum))
+print(
+    'Pour avoir le predator : La somme de {0} + {1} est {2}'.format(num1, num2, sum))
+
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 5
